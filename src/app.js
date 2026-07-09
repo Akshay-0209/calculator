984a39113b5277de60f0cdbc9c038c1061743173
Akshay-0209 <yahska.1992@gmail.com>
2026-07-09 16:34:06 +0530

    feat: update app.js to include dotenv, destructure errorHandler, and improve comments

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const path = require('path');
 const routes = require('./routes');
-const errorHandler = require('./middleware/errorHandler');
+const { errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 
@@ -12,13 +13,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Serve static files
+// Serve static frontend from /public
 app.use(express.static(path.join(__dirname, '../public')));
 
-// API Routes
+// API routes
 app.use('/api', routes);
 
-// Error handler
+// Error Handler
 app.use(errorHandler);
 
 module.exports = app;
